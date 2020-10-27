128f478b0d80028e559eb9173042b78c2d2962f3
Radu <cazacuraduic@gmail.com>
2020-10-27 13:17:21 +0200

    First commit, 1:17, 27 oct 2020, backend and front end in development

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import products from './data/products.js'
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API is running')
+})
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/api/product/:id', (req, res) => {
+  const product = products.find((item) => item._id === req.params.id)
+  res.json(product)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is up on ${process.env.NODE_ENV} port ${PORT}`.magenta.bold
+  )
+)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
-import './App.css'
+import React from 'react'
+
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
-  return <div className='App'></div>
+  return (
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route exact path='/' component={HomeScreen} />
+          <Route exact path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
